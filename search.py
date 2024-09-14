6b08320a6ff1fdc7237666fc3969c79124a168ae
Nividhitha99 <jvenkat1@umd.edu>
2024-09-14 05:54:10 -0400

    search function implement ENPM611

--- search.py
+++ search.py
@@ -1,8 +1,22 @@
 import json
+import re
 
 def search_json(json_data, search_string):
-    results = []
+       results = []
+       for each_data in json_data:
+           for key in each_data:
+              if (re.search(search_string, key)):
+            #if search_string in user_data:
+                results.append({key: each_data[key]})
+    
+       return results
+
+    
+           
+
+
+
     # Place your search code here
     # you will have to loop through the json_data file you created earlier
     # finally you can store the match in the result list and return it
-    return results
+    # return results
